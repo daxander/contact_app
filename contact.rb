61efb481a51314002d648badf3c8007e51df450e
David Feimer <davidfeimer@gmail.com>
2015-04-08 00:22:24 +0000

    Almost There

--- contact.rb
+++ contact.rb
@@ -8,60 +8,60 @@ class Contact
     @email = email
   end
  
-  def to_s
-    # TODO: return string representation of Contact
-    "#{@name},#{@email}"
-  end
+  # def to_s
+  #   # TODO: return string representation of Contact
+  #   "#{@name},#{@email}"
+  # end
 
   def to_array
     [@name, @email]
   end
-  
+
+
   ## Class Methods
   class << self
+    @@list_of_contacts = CSV.read('contacts.csv')
+
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
       new_contact = Contact.new(name, email) 
-      CSV.open('contacts.csv', 'a') do |csv|
-        # customers.array.each do |row_array|
-        csv << new_contact.to_array
+      @@list_of_contacts.each do |row|
+        if (row.include?(name) || row.include?(email))
+          return "This is a Duplicate Entry!" 
+        end
+      end
+      CSV.open('contacts.csv', 'a') do |row|
+      row << new_contact.to_array
       end
       new_contact
     end
  
     def find(search_term)
       # TODO: Will find and return contact by index
-      counter = 0
-      list_of_contacts = CSV.read('contacts.csv')
-      list_of_contacts.each do |row|
-        counter += 1
+      @@list_of_contacts.each_with_index do |row, real_index|
         if (row[0].include?(search_term) || row[1].include?(search_term))
-          puts "#{counter} : #{row[0]} (#{row[1]})" 
+          puts "#{real_index} : #{row[0]} (#{row[1]})" 
+        end
+        if search_term.to_i == real_index
+          puts "#{real_index} : #{row[0]} (#{row[1]})" 
         end
       end
-     
     end
  
     def all
       # TODO: Return the list of contacts, as is
-      counter = 1
-      CSV.foreach('contacts.csv') do |row|
-        puts "#{counter} : #{row[0]} (#{row[1]})"
-        counter += 1
+      @@list_of_contacts.each_with_index do |row, real_index|
+        puts "#{real_index} : #{row[0]} (#{row[1]})"
       end
     end
     
     def show(index)
       # TODO: Show a contact, based on ID
-      puts index
-      puts index.class
-      counter = 1
-      CSV.foreach('contacts.csv') do |row|
-        if counter == index 
-          puts "#{counter} : #{row[0]} (#{row[1]})"
+      @@list_of_contacts.each_with_index do |row, real_index|
+        if real_index == index 
+          puts "#{real_index} : #{row[0]} (#{row[1]})"
           return
         end
-          counter += 1
       end 
     end
     
--- contact_list.rb
+++ contact_list.rb
@@ -1,3 +1,4 @@
+require 'csv'
 require_relative 'contact'
 require_relative 'contact_database'
 
@@ -11,18 +12,15 @@ class ContactList
     new  - Create a new contact
     list - List all contacts
     show - Show a contact
-    find - Find a contact"
-    end
-
-    def self.input_for_string
-      puts "What is the string you want to search for?"
-      Contact.find(self.get_input)
+    find - Find a contact
+    phone - Add a Phone Number to a Contact
+    help - Show commands"
     end
 
     def self.get_input
       STDIN.gets.chomp
     end
-
+    
     def self.menu_select
       case @initial_input
       when 'new'
@@ -33,14 +31,20 @@ class ContactList
         Contact.show(@secondary_input.to_i)
       when 'find'
         Contact.find(@secondary_input)
+      when 'phone'
+      when 'help'
+        ContactList.menu
+      else
+        puts "That is an invalid command!"
+        ContactList.menu
       end
     end
 
     def self.new_contact
       puts "What is the contact's name?"
-      contact_name = self.get_input
+      contact_name = self.get_input.to_s
       puts "What is the contact's email?"
-      contact_email = self.get_input
+      contact_email = self.get_input.to_s
       puts Contact.create(contact_name, contact_email)
     end
 end
